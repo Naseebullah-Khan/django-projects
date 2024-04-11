71fd39819c7c254a341b3fea815c118671448d21
Naseeb Khan <naseebullah_hoshmand@hotmail.com>
2024-04-11 22:59:47 +0430

    Practiced Views & URL's

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import render
 
 # Create your views here.
+def projects(request):
+    return HttpResponse("list of projects")
+
+def singleProject(request, pk):
+    return HttpResponse(f"Project number {pk}")
